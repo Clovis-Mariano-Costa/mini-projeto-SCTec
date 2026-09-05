9260a6ab5136957540aef50f49eb0387f9a8672b
Clovis Mariano da Costa <aeonprimevo@gmail.com>
2026-09-05 19:45:06 -0300

    Documenta etapa de compatibilidade
    Documenta etapa de compatibilidade.
    A decisão de calcular a compatibilidade foi decidida e construída.

--- skillmatch.js
+++ skillmatch.js
@@ -59,3 +59,103 @@ function calcularCompatibilidade(candidato, vaga) {
 
     return percentual;
 }
+
+// ==============================
+// CLASSIFICAÇÃO DA COMPATIBILIDADE
+// ==============================
+
+function classificarCompatibilidade(percentual) {
+
+    if (percentual >= 80) {
+        return "Alta compatibilidade";
+    } else if (percentual >= 50) {
+        return "Média compatibilidade";
+    } else {
+        return "Baixa compatibilidade";
+    }
+}
+
+console.log(classificarCompatibilidade(100));
+console.log(classificarCompatibilidade(60));
+console.log(classificarCompatibilidade(30));
+
+
+
+// ==============================
+// HABILIDADES FALTANTES
+// ==============================
+
+function identificarHabilidadesFaltantes(candidato, vaga) {
+
+    const habilidadesFaltantes = vaga.requisitos.filter((requisito) => {
+        return !candidato.habilidades.includes(requisito.habilidade);
+    });
+
+    return habilidadesFaltantes;
+}
+
+// ==============================
+// MELHOR VAGA
+// ==============================
+
+function encontrarMelhorVaga(candidato, vagas) {
+
+    const melhorVaga = vagas.reduce((melhor, vagaAtual) => {
+
+        const compatibilidadeAtual = calcularCompatibilidade(
+            candidato,
+            vagaAtual
+        );
+
+        const compatibilidadeMelhor = calcularCompatibilidade(
+            candidato,
+            melhor
+        );
+
+        if (compatibilidadeAtual > compatibilidadeMelhor) {
+            return vagaAtual;
+        }
+
+        return melhor;
+
+    });
+
+    return melhorVaga;
+}
+
+// ==============================
+// RECOMENDAÇÃO DE ESTUDO
+// ==============================
+
+function criarRecomendacaoEstudo(candidato, vaga) {
+
+    const habilidadesFaltantes = identificarHabilidadesFaltantes(
+        candidato,
+        vaga
+    );
+
+    if (habilidadesFaltantes.length === 0) {
+        return "O candidato já possui todas as habilidades exigidas pela vaga.";
+    }
+
+    const habilidadePrioritaria = habilidadesFaltantes.reduce(
+        (maisImportante, habilidadeAtual) => {
+
+            if (habilidadeAtual.peso > maisImportante.peso) {
+                return habilidadeAtual;
+            }
+
+            return maisImportante;
+        }
+    );
+
+    return `Priorize o estudo de ${habilidadePrioritaria.habilidade}, pois essa habilidade possui peso ${habilidadePrioritaria.peso} nesta vaga.`;
+}
+
+
+const recomendacao = criarRecomendacaoEstudo(
+    candidato,
+    vagas[2]
+);
+
+console.log(recomendacao);
